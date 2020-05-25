0e3a4d3b6eeafe4efab416521186596e6392b87e
hxt-tg <782920705@qq.com>
2020-05-25 12:04:28 +0800

    Upload MP3Arrange

--- python/MP3Arrange/remove_d3.py
+++ python/MP3Arrange/remove_d3.py
@@ -1,6 +1,8 @@
 from mutagen.id3 import ID3
 import os
 
+MP3_DIR = './QQ'
+
 def modifyID3(filename, title=''):
     f = ID3(filename)
     f.delall('APIC')
@@ -17,6 +19,6 @@ def modifyID3(filename, title=''):
     f.delall('COMM')
     f.save()
 
-for fn in filter(lambda x: x.endswith('.mp3'), sorted(os.listdir('microsd'))):
+for fn in filter(lambda x: x.endswith('.mp3'), sorted(os.listdir(MP3_DIR))):
     if fn.startswith('0') or fn.startswith('1') or fn.startswith('2'): continue
-    modifyID3(f'microsd/{fn}')
+    modifyID3(f'{MP3_DIR}/{fn}')
--- python/MP3Arrange/write.py
+++ python/MP3Arrange/write.py
@@ -1,8 +1,11 @@
 import os
 from shutil import copyfile
 
+SRC_LIST = "filename_list/new_tmp_list_20200524.txt"
+DST_LIST = "filename_list/output_list.txt"
+
 SRC_DIR = 'microsd'
-DST_DIR = 'output'
+DST_DIR = 'H:/output'
 
 old_files = filter(lambda fn: fn.endswith('.mp3'), sorted(os.listdir(SRC_DIR)))
 old_files = {fn[:-4].split(' ')[-1] : fn for fn in old_files}
@@ -11,22 +14,27 @@ print("Old music total:", len(old_files))
 if not os.path.exists(DST_DIR):
     os.makedirs(DST_DIR)
 
+songs = set()
 idx = 0
-with open("output_list.txt", 'w', encoding='gbk') as fo:
-    with open("new_list.txt", encoding='gbk') as f:
+with open(DST_LIST, 'w', encoding='gbk') as fo:
+    with open(SRC_LIST, encoding='gbk') as f:
         for line in f.readlines():
             line = line.strip()
             if line == '': continue
             idx += 1
             line = line.split(' ')
             name = line[-1]
+            if name not in songs:
+                songs.add(name)
+            else:
+                raise ValueError(f"Duplicated song: {name}")
             src = f"{SRC_DIR}/{old_files[name]}"
             dst = f"{DST_DIR}/{idx:03d} {name}.mp3"
             fo.write(f"{idx:03d} {name}\n")
             if os.path.exists(dst):
                 continue
             print(f'{idx:03d} Copy from', f"'{src}'", 'to', f"'{dst}'")
-            # copyfile(src, dst)
+            copyfile(src, dst)
             if not os.path.exists(dst):
                 print("WARNING:", dst, "not copied.")
 
